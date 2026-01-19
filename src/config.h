8ab7b07be4b2172a68bc5289dc792c04bf3f31e0
ivan <i.foka@ostfalia.de>
2026-01-19 12:10:00 +0100

    Jetzt mit QoS1 PUBACK

--- src/config.h
+++ src/config.h
@@ -2,36 +2,40 @@
 #define CONFIG_H
 
 // ========== WLAN Konfiguration ==========
-//#define WIFI_SSID "TP-Link_2640"          // DEINE WLAN SSID
-//#define WIFI_PASSWORD "46813374"          // DEIN WLAN Passwort
-#define WIFI_TIMEOUT_MS 20000             // 20 Sekunden Timeout
-
+#define WIFI_SSID "TP-Link_2640"
+#define WIFI_PASSWORD "46813374"
+#define WIFI_TIMEOUT_MS 20000
 
 // ========== persönlicher Hotspot Konfiguration ==========
-#define WIFI_SSID "iPhone"          // DEINE Hotspot SSID
-#define WIFI_PASSWORD "egdM-frqL-6yyL-Xqww"          // DEIN  Hotspot Passwort
+//#define WIFI_SSID "iPhone"
+//#define WIFI_PASSWORD "egdM-frqL-6yyL-Xqww"
 
 // ========== NTP Konfiguration ==========
-#define NTP_SERVER "pool.ntp.org"         // NTP Server
-#define NTP_OFFSET_SECONDS 3600           // GMT+1 (Deutschland Winter)
-#define NTP_UPDATE_INTERVAL_MS 60000      // Alle 60 Sekunden aktualisieren
+#define NTP_SERVER "pool.ntp.org"
+#define NTP_OFFSET_SECONDS 3600
+#define NTP_UPDATE_INTERVAL_MS 60000
 
 // ========== Azure IoT Hub ==========
-#define IOT_HUB_HOSTNAME "iotHubIvanFoka.azure-devices.net"  // ← Dein IoT Hub Name!
+#define IOT_HUB_HOSTNAME "iotHubIvanFoka.azure-devices.net"
 #define DEVICE_ID "iotWeatherstationesp32"
-#define DEVICE_KEY "VCykuAaTiZdmGcRh+HTZXGCjbuy6TWUDSkWZFCzpdDY="  // ← Füge deinen Primary Key ein!
+#define DEVICE_KEY "VCykuAaTiZdmGcRh+HTZXGCjbuy6TWUDSkWZFCzpdDY="
 
 // MQTT Topics
 #define MQTT_TELEMETRY_TOPIC "devices/" DEVICE_ID "/messages/events/"
 #define MQTT_C2D_TOPIC "devices/" DEVICE_ID "/messages/devicebound/#"
 
+// ========== MQTT QoS Konfiguration ========== ✅ NEU!
+#define MQTT_QOS_LEVEL 1              // 0=keine Bestätigung, 1=PUBACK, 2=PUBCOMP
+//#define MQTT_PORT 8883                // Azure IoT Hub MQTT Port (TLS)
+//#define RECONNECT_INTERVAL 5000       // Reconnect alle 5 Sekunden
+
 // ========== Sensor Konfiguration ==========
-#define SENSOR_READ_INTERVAL_MS 5000      // Sensoren alle 5 Sekunden auslesen
+#define SENSOR_READ_INTERVAL_MS 5000
 
 // ========== LED Pin ==========
-#define LED_PIN 23                         // Onboard LED (GPIO2)
+#define LED_PIN 23
 
 // ========== Debug Level ==========
-#define DEBUG_LEVEL 3                      // 0=None, 1=Error, 2=Warn, 3=Info, 4=Debug
+#define DEBUG_LEVEL 3
 
 #endif
